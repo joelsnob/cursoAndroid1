91fb02e84f15f30e5117b9c72e20f0cef9c0795f
Joel Aragão <joel.aragao@fpf.br>
2014-09-30 12:58:08 -0400

    adicionado exercicio aula1.

---
+++ modulo1/aula1/exercicios/Aula1-Exercicio1/src/curso/android/exercicio/Activity1.java
@@ -0,0 +1,31 @@
+package curso.android.exercicio;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class Activity1 extends Activity implements OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		//Deve criar o botão
+		
+		//Deve ser definido o texto com base em um resource
+		
+		//Deve adiciona o listener que será chamado em caso de clique no botão
+		
+		//Definir o botão como layout da activity
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		//Quando o botão clicado, cria uma intent
+		
+		//Deve Invoca a Activity2
+
+	}
+}
---
+++ modulo1/aula1/exercicios/Aula1-Exercicio1/src/curso/android/exercicio/Activity2.java
@@ -0,0 +1,19 @@
+package curso.android.exercicio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity2 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		//Deve ser criado um ImageView, para exibir uma imagem
+		
+		//Definir qual imagem será exibida com base em um drawable resource
+		
+		//Definir o ImageView como a view da activity
+		
+	}
+}
